a7d8f497542270f4b51917fa60a91bfb49e847f7
K Harsh <harshnaidu2u@gmail.com>
2025-10-06 19:45:18 +0530

    connecting: wiring all comps, toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
 import EmployeeTable from './components/EmployeeTable'
+import Modal from './screens/Modal'
 
 function App() {
 
   return (
     <>
-      <EmployeeTable />
+      <div className="min-h-screen bg-gray-100 flex justify-center items-start pt-10 px-4">
+        <div className="w-full max-w-5xl bg-white rounded-lg shadow-md p-6">
+          <EmployeeTable />
+          <Modal />
+        </div>
+      </div>
     </>
   )
 }
